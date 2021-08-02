d6a4ce895fe820a99008a347ca774bb5097e9b12
linbudu599 <linbudu599@gmail.com>
2021-08-02 20:53:02 +0800

    fix: remove templates

--- src/middlewares/templates/egg-middleware-functional.ts
+++
@@ -1,6 +0,0 @@
-import { IWebMiddleware, IMidwayWebNext } from '@midwayjs/web';
-import { Context } from 'egg';
-
-export const Middleware = () => {
-  return async function (ctx: Context, next: IMidwayWebNext) {};
-};
